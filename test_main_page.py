086f730787eec9885ece98daaf1bc1a0dd81879d
Mary_Po <pnmry@yandex.ru>
2023-11-22 11:08:55 +0300

    Добавлен тест заполнения формы обращения с allpairs

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,8 @@ from .pages.register_page import RegisterPage
 from .pages.visually_impaired_page import VisuallyImpairedPage
 from .pages.appeal_page import AppealPage
 import allure
+import allpairspy
+from allpairspy import AllPairs
 
 @pytest.mark.chrome
 @allure.epic("Chrome tests for Main page")
@@ -13,7 +15,44 @@ import allure
 @allure.feature("Main page menu")
 @allure.suite("Main page menu")
 class TestHappyPathChrome():
-    @pytest.mark.search
+    @pytest.mark.reg
+    @allure.story("User registration")
+    @allure.sub_suite("User registration")
+    @allure.title("Register new user")
+    def test_register_new_user(self, browser_chrome):
+        link = "http://academy21.ru/" 
+        login = "testik"
+        email = "test"
+        password = "test"
+        with allure.step("Step 1: open main page"):
+            main_page = BasePage(browser_chrome, link)
+            main_page.open()
+            main_page.should_be_correct_response_status_code()
+        with allure.step("Step 2: go to register page"):
+            main_page.go_to_register_page()
+            register_page = RegisterPage(browser_chrome, browser_chrome.current_url)
+        with allure.step("Step 3: register new user"):
+            register_page.register_new_user(login, email, password)
+
+    @pytest.mark.login
+    @allure.story("User login")
+    @allure.sub_suite("User login")
+    @allure.title("Log in, go to profile, log out")
+    def test_login_go_to_profile_logout(self, browser_chrome):
+        link = "http://academy21.ru/" 
+        name = "test"
+        password = "test"
+        with allure.step("Step 1: open main page"):
+            main_page = BasePage(browser_chrome, link)
+            main_page.open()
+            main_page.should_be_correct_response_status_code()
+        with allure.step("Step 2: log in"):
+            main_page.log_in(name, password)
+        with allure.step("Step 3: go to user profile"):
+            main_page.go_to_user_profile(name)
+        with allure.step("Step 4: log out"):
+            main_page.log_out()@pytest.mark.search
+    
     @allure.story("Search")
     @allure.sub_suite("Search")
     @allure.title("Search by string and button")
@@ -151,29 +190,10 @@ class TestHappyPathChrome():
         with allure.step("Step 4: go to search result page"):
             main_page.should_be_search_request_in_search_string(search_request)
 
-    @pytest.mark.reg
-    @allure.story("User registration")
-    @allure.sub_suite("User registration")
-    @allure.title("Register new user")
-    def test_register_new_user(self, browser_chrome):
-        link = "http://academy21.ru/" 
-        login = "testik"
-        email = "test"
-        password = "test"
-        with allure.step("Step 1: open main page"):
-            main_page = BasePage(browser_chrome, link)
-            main_page.open()
-            main_page.should_be_correct_response_status_code()
-        with allure.step("Step 2: go to register page"):
-            main_page.go_to_register_page()
-            register_page = RegisterPage(browser_chrome, browser_chrome.current_url)
-        with allure.step("Step 3: register new user"):
-            register_page.register_new_user(login, email, password)
-
     @pytest.mark.ask
     @allure.story("Appeal")
     @allure.sub_suite("Appeal")
-    @allure.title("Send new appeal")
+    @allure.title("Go to appeal page and create single appeal")
     def test_send_new_appeal(self, browser_chrome):
         link = "http://academy21.ru/" 
         input = "test"
@@ -188,23 +208,29 @@ class TestHappyPathChrome():
             main_page.go_to_appeal_page()
             appeal_page = AppealPage(browser_chrome, browser_chrome.current_url)
         with allure.step("Step 3: create new appeal"):
-            appeal_page.create_new_appeal(input, phone, file_path)
-
-    @pytest.mark.login
-    @allure.story("User login")
-    @allure.sub_suite("User login")
-    @allure.title("Log in, go to profile, log out")
-    def test_login_go_to_profile_logout(self, browser_chrome):
-        link = "http://academy21.ru/" 
-        name = "test"
-        password = "test"
-        with allure.step("Step 1: open main page"):
-            main_page = BasePage(browser_chrome, link)
-            main_page.open()
-            main_page.should_be_correct_response_status_code()
-        with allure.step("Step 2: log in"):
-            main_page.log_in(name, password)
-        with allure.step("Step 3: go to user profile"):
-            main_page.go_to_user_profile(name)
-        with allure.step("Step 4: log out"):
-            main_page.log_out()
+            appeal_page.create_single_appeal(input, phone, file_path)
+            
+    @pytest.mark.allpairs
+    @allure.story("Appeal")
+    @allure.sub_suite("Appeal")
+    @allure.title("Create all combinations of appeal")
+    @pytest.mark.parametrize(["addressee_item", "type_item", "topic_item", "who_item"], [
+        values for values in AllPairs([
+            [1, 2],
+            [2, 3],
+            [2, 3],
+            [2, 3]
+        ])
+    ])
+    def test_send_all_pairs_appeal(self, browser_chrome, addressee_item, type_item, topic_item, who_item):
+        link = "http://appeal.academy21.ru/" 
+        input = "test"
+        phone = "1111111111"
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+        file_path = os.path.join(current_dir, 'test_data', 'test_image.jpg')
+        with allure.step("Step 1: open appeal page"):
+            appeal_page = AppealPage(browser_chrome, link)
+            appeal_page.open()
+            appeal_page.should_be_correct_response_status_code()
+        with allure.step("Step 2: create all combinations of appeal"):
+            appeal_page.create_all_pairs_appeal(input, phone, file_path, addressee_item, type_item, topic_item, who_item)
